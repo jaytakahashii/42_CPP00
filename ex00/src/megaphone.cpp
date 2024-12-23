cc1472a389c0a5833c070323a4f11537b131017e
JayTakahashi <127118277+jaytakahashii@users.noreply.github.com>
2024-12-23 21:29:19 +0900

    feat(SRC): ex00 (#1)
    * feat(CI): ✨  edit clang-format directory
    
    * feat(SRC): ✨  add ex00
    
    * fix(SRC): 👾  最後に空白が入ってしまう問題を解決
    
    * feat(SRC): ✨  add test
    
    * feat(CI): ✨  add test
    
    * refactor(CI): ♻️  only pull_request
    
    * fix(CI): 👾  fix no such file or directory
    
    * fix(CI): 👾  fix no such file ??
    
    * refactor(CI): ♻️  debug ls ./test
    
    * fix(CI): 👾  fix name error

---
+++ ex00/src/megaphone.cpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jay <jay@student.42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/23 18:24:54 by jay               #+#    #+#             */
+/*   Updated: 2024/12/23 20:26:55 by jay              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <cctype>
+#include <iostream>
+
+int main(int argc, char **argv) {
+  if (argc > 1) {
+    for (int i = 1; i < argc; i++) {
+      for (int j = 0; argv[i][j] != '\0'; j++)
+        argv[i][j] = (char)toupper(argv[i][j]);
+      std::cout << argv[i];
+      if (i < argc - 1) std::cout << " ";
+    }
+    std::cout << std::endl;
+  } else
+    std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+  return (0);
+}
