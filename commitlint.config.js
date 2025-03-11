f9a76288c9756a2baee5485376f9e6f11bd5fdae
Jay Takahashi (42ubuntu) <qsakura14555@gmail.com>
2025-03-11 16:38:53 +0900

    refactor(ENV): ♻️  comment JP -> EN

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,21 +1,14 @@
 module.exports = {
-  extends: ['@commitlint/config-conventional'],
+  extends: ["@commitlint/config-conventional"],
   rules: {
-    'type-enum': [
-      2, // エラー (1: 警告, 0: 無視)
-      'always',
-      ['feat', 'fix', 'refactor', 'Revert'],
+    "type-enum": [
+      2, // error: 2, warning: 1, off: 0
+      "always",
+      ["feat", "fix", "refactor", "Revert"],
     ],
-    'scope-enum': [
-      2,
-      'always',
-      ['SRC', 'DOC', 'CI', 'ENV'],
-    ],
-    // コミットメッセージの最大長を設定
-    'header-max-length': [2, 'always', 64],
-    // スコープを必須とする
-    'scope-empty': [2, 'never'],
-    // サブジェクトを必須とする
-    'subject-empty': [2, 'never'],
+    "scope-enum": [2, "always", ["SRC", "DOC", "CI", "ENV"]],
+    "header-max-length": [2, "always", 64],
+    "scope-empty": [2, "never"],
+    "subject-empty": [2, "never"],
   },
 };
