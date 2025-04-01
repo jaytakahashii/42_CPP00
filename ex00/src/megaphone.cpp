4643b93e746bbafd5aa61255efc378d4317ffcbb
Jay Takahashi <qsakura14555@gmail.com>
2025-04-01 15:21:06 +0900

    refactor(SRC): ♻️  use std::toupper and string

--- ex00/src/megaphone.cpp
+++ ex00/src/megaphone.cpp
@@ -3,9 +3,9 @@
 int main(int argc, char **argv) {
   if (argc > 1) {
     for (int i = 1; i < argc; i++) {
-      for (int j = 0; argv[i][j] != '\0'; j++)
-        argv[i][j] = std::toupper(argv[i][j]);
-      std::cout << argv[i];
+      std::string str(argv[i]);
+      for (size_t j = 0; j < str.length(); j++)
+        std::cout << (char)std::toupper(str[j]);
     }
     std::cout << std::endl;
   } else
